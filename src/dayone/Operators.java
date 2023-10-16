fce8918cc05b067b9afee34b338370ce37984475
arunprakash17 <101006090+arunprakash17@users.noreply.github.com>
2023-10-17 00:02:02 +0530

    Commit

--- src/dayone/Operators.java
+++ src/dayone/Operators.java
@@ -3,7 +3,13 @@ package dayone;
 public class Operators {
 
 	public static void main(String[] args) {
-		
+		//Arithmetic operators
+		float a = 5, b = 3;
+		System.out.println("The Addition is "+ (a+b));
+		System.out.println("The substraction is "+ (a-b));
+		System.out.println("The multiplication is "+ (a*b));
+		System.out.println("The division is "+(a/b));
+		System.out.println("The modulus is "+ (a%b));
 	}
 
 }
